8e07400b1b30ebb4a0ed5f681ee0ef6b27c7fe1c
Izuyah <egoz321@naver.com>
2023-04-02 18:24:56 +0900

    노트북 로컬 자료 전송 오류 해결

--- class-react-app/src/App.js
+++ class-react-app/src/App.js
@@ -1,25 +1,93 @@
+import React, {useState, useEffect} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <h1>hello world</h1>
+        <FuncComp initNumber={2}></FuncComp>
+        <ClassComp initNumber={2}></ClassComp>
     </div>
   );
+}
+
+var funcStyle = 'color:blue';
+var funcId = 0;
+function FuncComp(props){
+  var [number, setNumber] = useState(props.initNumber);
+  var [date, setDate] = useState(new Date().toString());
+
+  useEffect(function(){
+    console.log('%cfunc => useEffect '+(++funcId), funcStyle);
+    document.title = number + ' : ' +date;
+  });
+  
+  console.log('%cfunc => render ' +(++funcId), funcStyle);
+  return(
+    <div className="container">
+      <h2>function style component</h2>
+      <p>Number : {number}</p>
+      <p>Date : {date}</p>
+      <input type="button" value="random" onClick={
+        function(){
+          setNumber(Math.random());
+        }
+      }></input>
+      <input type="button" value="date" onClick={
+        function(){
+          setDate(new Date().toString());
+        }
+      }></input>
+    </div>
+  )
+}
+
+var classStyle = 'color:red';
+class ClassComp extends React.Component{
+  state = {
+    number:this.props.initNumber,
+    date:(new Date()).toString()
+  }
+
+  componentWillMount(){
+    console.log('%cclass => componentWillMount', classStyle);
+  }
+  componentDidMount(){
+    console.log('%cclass => componentDidMount', classStyle);
+  }
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('%cclass => shouldComponentUpdate', classStyle);
+    return true;
+  }
+  componentWillUpdate(nextProps, nextState){
+    console.log('%cclass => componentWillUpdate', classStyle);
+  }
+  componentDidUpdate(nextProps, nextState){
+    console.log('%cclass => componentDidUpdate', classStyle);
+  }
+
+  render(){
+    console.log('%cclass => render', classStyle);
+    return(
+      <div className='container'>
+        <h2>class style component</h2>
+        <p>Number : {this.state.number}</p>
+        <p>Date : {this.state.date}</p>
+        <input type="button" value="random" onClick={
+          function(){
+            this.setState({number:Math.random()});
+          }.bind(this)
+        }></input>
+        <input type="button" value="date" onClick={
+          function(){
+            this.setState({date:(new Date()).toString()})
+          }.bind(this)
+        }></input>
+      </div>
+      
+    )
+  }
 }
 
 export default App;
